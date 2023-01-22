ebfa08d1051ac4c82ee3a82cc33450acf72173f6
masato-kobayashi5427 <savracoco@gmail.com>
2023-01-22 22:35:44 +0900

    first commit

---
+++ db/migrate/20230119010634_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+
+      t.string :nickname, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :last_name, null: false
+      t.string :first_name, null: false
+      t.string :last_name_kana, null: false
+      t.string :first_name_kana, null: false
+      t.date :birth_day, null: false
+      t.text :introduction, null: false
+
+      t.timestamps
+    end
+  end
+end
