97c02d17eaa284321998dd8025ec562562abfd4e
masato-kobayashi5427 <savracoco@gmail.com>
2023-02-05 22:44:40 +0900

    コメント保存機能の実装

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,4 +1,8 @@
 class Chat < ApplicationRecord
+  validates :content, presence: true
+  validates :user, presence: true
+  validates :episode_room, presence: true
+
   belongs_to :user
   belongs_to :episode_room
 end
