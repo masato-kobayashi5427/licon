5c6ab261dcb20b720111d18c190af98f019d50bb
masato-kobayashi5427 <savracoco@gmail.com>
2023-02-20 21:51:57 +0900

    フォームオブジェクトの実装

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,6 @@
 class Order < ApplicationRecord
   attr_accessor :token, :order
 
-  validates :user, presence: true
-  validates :episode, presence: true
-  validates :price, presence: true
-  validates :token, presence: true
-
   belongs_to :user
   belongs_to :episode
   has_one :episode_room
