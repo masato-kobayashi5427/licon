4bf9dbb68a8f4604b7cfa20715f5bcc50ccbe3b6
masato-kobayashi5427 <savracoco@gmail.com>
2023-03-03 15:51:35 +0900

    チャット機能のバリデーションの修正

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -5,8 +5,16 @@ class Chat < ApplicationRecord
   # validates :content, presence: true
   validates :user, presence: true
   validates :episode_room, presence: true
+  validate :at_least_one_field_present
 
   has_one_attached :image
   belongs_to :user
   belongs_to :episode_room
+
+  private
+  def at_least_one_field_present
+    if content.present? && image.attached? && canvasUrl.present?
+      errors.add(:base, "content, image_url, or canvasUrl must be present")
+    end
+  end
 end
--- app/models/episode_room.rb
+++ app/models/episode_room.rb
@@ -1,8 +1,5 @@
 class EpisodeRoom < ApplicationRecord
-  # validates :name, presence: true
-  # validates :episode, presence: true
-  # validates :order, presence: true
-
+  
   belongs_to :episode
   belongs_to :order
   has_many :episode_room_users
