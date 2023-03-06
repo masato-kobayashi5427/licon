3a3b26ef15dbee5203eb4480a68649221a8a3b3f
masato-kobayashi5427 <savracoco@gmail.com>
2023-03-06 12:58:41 +0900

    セッションの設定の変更

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
-if Rails.env === 'production'
-  Rails.application.config.session_store :cookie_store, key: 'licon', domain: 'http://localhost:3000'
+if Rails.env.production?
+  Rails.application.config.session_store :cookie_store, key: 'licon', domain: 'licon.vercel.app', secure: true
 else
   Rails.application.config.session_store :cookie_store, key: 'licon'
 end
