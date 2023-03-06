ef6e3060473f8cee0671c8e92cc6e05828ae2a0a
masato-kobayashi5427 <savracoco@gmail.com>
2023-03-06 17:17:37 +0900

    same_siteの設定

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_session_id', same_site: :strict, secure: Rails.env.production?
+Rails.application.config.session_store :cookie_store, key: '_session_id', same_site: :lax, secure: Rails.env.production?
