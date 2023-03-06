50f0734d767ceedc72a59579a1b7b8022bf3e2bf
masato-kobayashi5427 <savracoco@gmail.com>
2023-03-06 15:49:32 +0900

    セッションの修正。

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: 'session_id', domain: :all, tld_length: 2
+Rails.application.config.session_store :cookie_store, key: '_session_id', domain: :all, secure: true
