77e31676c2f98ccced0d232896d39e978939c83f
masato-kobayashi5427 <savracoco@gmail.com>
2023-03-08 17:00:26 +0900

    ローカル環境のセッションの適用

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,1 @@
-if Rails.env.production?
-  Rails.application.config.session_store :cookie_store, key: '_session_id', same_site: :none, secure: Rails.env.production?
-else
-  Rails.application.config.session_store :cookie_store, key: 'licon'
-end
+Rails.application.config.session_store :cookie_store, key: '_session_id', same_site: :lax, secure: true
