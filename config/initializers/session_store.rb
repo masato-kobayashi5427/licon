5bd96c4dff0ad971c663531c06d1526d92cc5ea3
masato-kobayashi5427 <savracoco@gmail.com>
2023-03-08 14:43:02 +0900

    本番とローカルの設定の分離

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: '_session_id', same_site: :none, secure: Rails.env.production?
+if Rails.env.production?
+  Rails.application.config.session_store :cookie_store, key: '_session_id', same_site: :none, secure: Rails.env.production?
+else
+  Rails.application.config.session_store :cookie_store, key: 'licon'
+end
