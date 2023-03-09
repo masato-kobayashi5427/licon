d48e859d87d67476ca69fb94a505df59b1af3917
masato-kobayashi5427 <savracoco@gmail.com>
2023-03-09 14:26:25 +0900

    ログの開始

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -8,16 +8,16 @@ class ChatChannel < ApplicationCable::Channel
 
   def chat(data)
     message = { sender: current_user.nickname, body: data['body'] }
-
     # 一時的にメッセージを保持する
     @messages ||= []
     @messages << message
-
+    Rails.logger.debug @messages
     ActionCable.server.broadcast("chat#{params[:room_id]}", message)
   end
 
   def reconnect
     # 再接続時に保持したメッセージを再送信する
+    Rails.logger.debug @messages
     @messages&.each { |message| ActionCable.server.broadcast("chat#{params[:room_id]}", message) }
   end
 end
