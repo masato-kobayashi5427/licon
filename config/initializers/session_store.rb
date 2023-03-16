4fe8dd2d7f65143597c9ed3685e6568bc5bf2991
masato <114460411+masato-kobayashi5427@users.noreply.github.com>
2023-03-17 07:52:33 +0900

    Update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_session_id', same_site: :lax, secure: true
+Rails.application.config.session_store :cookie_store, key: '_session_id', same_site: :none, secure: true
